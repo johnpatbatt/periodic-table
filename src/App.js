9cb927ef7f8f997a0de943762b83be58098f8059
johnpatbatt <dev.john.batt@gmail.com>
2019-03-12 18:49:24 -0400

    some functions

--- src/App.js
+++ src/App.js
@@ -6,15 +6,78 @@ import "./App.css";
 const elements = elementsJSON.elements;
 
 class App extends Component {
+  state = {
+    molecule: [],
+    formula: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  addtomolecule = element => {
+    const arr = this.state.molecule;
+    arr.push(element);
+    this.setState({
+      molecule: arr,
+      formula: this.getformula(this.state.molecule),
+      molecular_weight: this.getmolecularweight(this.state.molecule)
+    });
+    console.log(arr);
+  };
+
+  getformula = elements => {
+    let count = {};
+    let formula = "";
+
+    elements.map(element => {
+      if (!count[element.symbol]) {
+        count[element.symbol] = 1;
+      } else {
+        count[element.symbol]++;
+      }
+    });
+    for (let key in count) {
+      formula += key;
+
+      if (count[key] === 1) {
+      } else {
+        formula += count[key];
+      }
+    }
+
+    return formula;
+  };
+
+  getmolecularweight = elements => {
+    let molecular_weight = 0;
+
+    elements.map(element => {
+      molecular_weight += element.atomic_mass;
+    });
+
+    return molecular_weight;
+  };
+
   render() {
     return (
       <div className="App">
+        <p>Formula: {this.state.formula}</p>
+
+        <p>Molecular Weight: {this.state.molecular_weight}</p>
         <div class="grid-container">
           {elements
             .slice(0, 57)
             .concat(elements.slice(71, 89), elements.slice(103, -1))
             .map(element => {
-              return <Element element={element} />;
+              return <Element element={element} add={this.addtomolecule} />;
             })}
         </div>
 
--- src/components/Element/index.js
+++ src/components/Element/index.js
@@ -3,15 +3,20 @@ import "./style.css";
 
 function Element(props) {
   return (
-    <div class={`grid-item ${props.element.symbol} `}>
-    <p class="atomicnumber">{props.element.number}</p>
-    {props.element.symbol}
-    <p class="massnumber">
-      {props.element.atomic_mass % 1 === 0
-        ? props.element.atomic_mass
-        : props.element.atomic_mass.toFixed(3).toString()}
-    </p>
-  </div>
+    <div
+      onClick={() => {
+        props.add(props.element);
+      }}
+      class={`grid-item ${props.element.symbol} `}
+    >
+      <p class="atomicnumber">{props.element.number}</p>
+      {props.element.symbol}
+      <p class="massnumber">
+        {props.element.atomic_mass % 1 === 0
+          ? props.element.atomic_mass
+          : props.element.atomic_mass.toFixed(3).toString()}
+      </p>
+    </div>
   );
 }
 
